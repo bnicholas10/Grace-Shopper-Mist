2102da58bf4dfb167bf5576e66e0702f210c0b00
Bpc21 <b.pereda@hotmail.com>
2022-11-03 20:56:02 -0400

    games.js / users.js in db
    db functions

--- db/games.js
+++ db/games.js
@@ -1,14 +1,130 @@
-const client = require(".");
+const client = require("./client");
 
-const fetchGames = async () => {
-  const response = await client.query(`
-    SELECT * FROM games;
+async function getGameById(gameId) {
+  try {
+    const {
+      rows: [game],
+    } = await client.query(
+      `
+      SELECT *
+      FROM games
+      WHERE id=$1;
+    `,
+      [gameId]
+    );
+
+    if (!game) {
+      throw {
+        name: "GameNotFoundError",
+        message: "Could not find a game with that gameId",
+      };
+    }
+
+    return game;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getGameByName(name) {
+  try {
+    const {
+      rows: [game],
+    } = await client.query(
+      `
+      SELECT *
+      FROM games
+      WHERE name=$1;
+    `,
+      [name]
+    );
+
+    if (!game) {
+      throw {
+        name: "GameNotFoundError",
+        message: "Could not find a game with that name",
+      };
+    }
+
+    return game;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllGames() {
+  try {
+    const { rows: gameIds } = await client.query(`
+      SELECT id
+      FROM games;
     `);
-  return response.rows;
-};
+
+    const games = await Promise.all(
+      gameIds.map((game) => getGameById(game.id))
+    );
+
+    return games;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createGame({
+  name,
+  price,
+  publisher,
+  description,
+  rating,
+  category,
+}) {
+  try {
+    const {
+      rows: [game],
+    } = await client.query(
+      `
+      INSERT INTO games(name, price, publisher, description, rating, category ) 
+      VALUES($1, $2, $3, $4, $5, $6)
+      RETURNING *;
+    `,
+      [name, price, publisher, description, rating, category]
+    );
+
+    return game;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateGame(gameId, fields = {}) {
+  // build the set string
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  try {
+    // update any fields that need to be updated
+    if (setString.length > 0) {
+      await client.query(
+        `
+        UPDATE games
+        SET ${setString}
+        WHERE id=${gameId}
+        RETURNING *;
+      `,
+        Object.values(fields)
+      );
+    }
+
+    return await getGameById(gameId);
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = {
-  fetchGames,
+  getGameById,
+  getGameByName,
+  getAllGames,
+  createGame,
+  updateGame,
 };
-
-// comment from bernie to see if it works
--- db/index.js
+++ db/index.js
@@ -16,4 +16,6 @@ const client = new Pool(
       }
 );
 
-module.exports = client;
+module.exports = {
+  client,
+};
