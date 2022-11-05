debf6e9587a4943c5345387134a2a362dc3ada78
Diana Clemente <dianaeclemente@gmail.com>
2022-11-05 09:57:57 -0400

    test api/publishers add

--- db/index.js
+++ db/index.js
@@ -16,6 +16,40 @@ const client = new Pool(
       }
 );
 
+async function getAllPublishers() {
+  try {
+    const { rows } = await client.query(`
+      SELECT *
+      FROM publishers;
+    `);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getGamesByPublisherName(publisherName) {
+  try {
+    const { rows: gameIds } = await client.query(
+      `
+        SELECT games.id
+        FROM games
+        JOIN game_publishers ON games.id=game_publishers."gameId"
+        JOIN publishers ON publishers.id=game_publishers."publisherId"
+        WHERE publishers.name=$1;
+      `,
+      [publisherName]
+    );
+
+    return await Promise.all(gameIds.map((game) => getGameById(game.id)));
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
+  getGamesByPublisherName,
+  getAllPublishers,
 };
